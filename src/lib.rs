e30b5c3db13c8025d6dafa03f1979957fbd8cb9e
ph4u <1057021+ph4un00b@users.noreply.github.com>
2023-06-23 10:56:39 -0600

    feat(lex): :fire: init

---
+++ src/lib.rs
@@ -0,0 +1,34 @@
+mod lexer_01;
+
+#[cfg(test)]
+mod tests {
+    use crate::lexer_01::Lexer;
+    use crate::lexer_01::TokenType;
+
+    #[test]
+    fn it_works() {
+        let input = "=+(),{};";
+
+        let tests = [
+            (TokenType::ASSIGN, b'='),
+            (TokenType::PLUS, b'+'),
+            (TokenType::LPAREN, b'('),
+            (TokenType::RPAREN, b')'),
+            (TokenType::COMMA, b','),
+            (TokenType::LBRACE, b'{'),
+            (TokenType::RBRACE, b'}'),
+            (TokenType::SEMICOLON, b';'),
+            (TokenType::EOF, b'\0'),
+        ];
+
+        let mut lex = Lexer::new(input.into());
+
+        for (expected_token, expected_literal) in tests {
+            let tok = lex.next_tok();
+            assert_eq!(tok.kind, expected_token);
+            println!("💃 {:?}, expected {:?}", tok.kind, expected_token);
+            println!("🕺 {:?}, expected {:?}", tok.literal, expected_literal);
+            assert_eq!(tok.literal, expected_literal);
+        }
+    }
+}
