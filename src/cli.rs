a3e30f216d3dfe3b8c11838d7af445d9e5c5e802
ph4u <1057021+ph4un00b@users.noreply.github.com>
2023-06-28 17:04:09 -0600

    + working repl: chore

--- src/cli.rs
+++ src/cli.rs
@@ -43,8 +43,8 @@ impl Lexer {
                 io::stdout().flush()?;
                 let line = lines.next().transpose()?;
                 if let Some(line) = line {
-                    let l = Lexer::new(line).tokens;
-                    println!("👀 \n\n{:?}", l);
+                    let tokens = Lexer::new(line).tokens;
+                    println!("👀 \n\n{:?}", tokens);
                     //? We need to reset this flag in the interactive loop.
                     //? If the user makes a mistake,
                     //? it shouldn’t kill their entire session.
