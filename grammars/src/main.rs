18a64270b4231267f03cd3d567a9b2bb4478d4bb
ph4u <1057021+ph4un00b@users.noreply.github.com>
2023-07-05 11:48:48 -0600

    chore(grammar):

--- grammars/src/main.rs
+++ grammars/src/main.rs
@@ -1,3 +1,16 @@
+use pest::Parser;
+use pest_derive::Parser;
+
+#[derive(Parser)]
+#[grammar = "csv.pest"]
+pub struct CSVParser;
+
+//* if you run `cargo doc`, it generates
+//* CSVParser::parse and an enum called Rule with a single variant Rule::field
 fn main() {
-    println!("Hello, world!");
+    let successful_parse = CSVParser::parse(Rule::field, "-273.15");
+    println!("{:?}", successful_parse);
+
+    let unsuccessful_parse = CSVParser::parse(Rule::field, "this is not a number");
+    println!("{:?}", unsuccessful_parse);
 }
