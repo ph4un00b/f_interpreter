9800d7c5e6015acb450c362feb4d30c48f392463
ph4u <1057021+ph4un00b@users.noreply.github.com>
2023-07-05 13:49:00 -0600

    + simple csv

--- grammars/src/main.rs
+++ grammars/src/main.rs
@@ -1,16 +1,37 @@
+use std::fs;
+
 use pest::Parser;
 use pest_derive::Parser;
 
 #[derive(Parser)]
-#[grammar = "csv.pest"]
+#[grammar = "csv2.pest"] //? cargo doc
 pub struct CSVParser;
 
 //* if you run `cargo doc`, it generates
 //* CSVParser::parse and an enum called Rule with a single variant Rule::field
 fn main() {
-    let successful_parse = CSVParser::parse(Rule::field, "-273.15");
-    println!("{:?}", successful_parse);
+    let successful_parse = CSVParser::parse(Rule::field, "1,2").unwrap();
+    let tokens = successful_parse.clone().tokens();
 
-    let unsuccessful_parse = CSVParser::parse(Rule::field, "this is not a number");
+    for token in tokens {
+        println!("{:?}", token);
+    }
+
+    let successful_parse = CSVParser::parse(Rule::record, "2,3").unwrap();
+    let tokens = successful_parse.clone().tokens();
+
+    for token in tokens {
+        println!("{:?}", token);
+    }
+
+    let unsuccessful_parse: Result<pest::iterators::Pairs<'_, Rule>, pest::error::Error<Rule>> =
+        CSVParser::parse(
+            Rule::file,
+            r#"1,2
+3,4
+5,6
+7,8
+"#, //? last line needed
+        );
     println!("{:?}", unsuccessful_parse);
 }
