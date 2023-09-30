6543868aa0d5d41f11d993dd2be94bf927ce4cff
ph4u <1057021+ph4un00b@users.noreply.github.com>
2023-09-29 21:19:07 -0600

    ferrous: sharable cell

--- examples/smart-pointers-cell.rs
+++ examples/smart-pointers-cell.rs
@@ -16,4 +16,19 @@ fn main() {
     assert_eq!(*p2_shared, 42 + 27);
     let p1_shared: &i32 = unsafe { &*alias1.get() };
     assert_eq!(*p1_shared, *p2_shared);
+
+    // * refcell
+    let x: std::cell::RefCell<i32> = std::cell::RefCell::new(42);
+    let (alias1, alias2) = (&x, &x);
+
+    let mut p1_exclusive = alias1.borrow_mut();
+    *p1_exclusive += 27;
+    drop(p1_exclusive);
+
+    let p2_shared = alias2.borrow();
+    assert_eq!(*p2_shared, 42 + 27);
+    //? This isn't allowed here:
+    // let p2_mutable = alias2.borrow_mut();
+    let p1_shared = alias1.borrow();
+    assert_eq!(*p1_shared, *p2_shared);
 }
