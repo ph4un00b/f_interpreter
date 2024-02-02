aac06bd6a92f433acbf9f2cfe03a4b9b66d9cd89
ph4u <1057021+ph4un00b@users.noreply.github.com>
2024-02-01 18:19:40 -0600

    chore: rm examples

--- examples/smart-pointers-box.rs
+++
@@ -1,9 +0,0 @@
-use bytes::Bytes;
-use std::collections::HashMap;
-fn main() {
-    let mut kv = HashMap::new();
-    kv.insert("jamon".to_string(), Bytes::from(vec![0, 2, 4]));
-    let boxed = Box::new(kv);
-
-    println!("BOX GOT {:?}", *boxed);
-}
